ee8e58ca8cee04bb89a81adae86417721ae44d0e
samaakhalifa21 <samaakhalifa19@gmail.com>
2023-09-04 20:21:36 +0300

    first commit

--- app/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-use app\controllers\ProductController;
-use app\controllers\HomeControllers;
-use Sectheater\http\Route;
-use Sectheater\DB;
-
-
-Route::get('/',[ProductController::class,'index']);
--- app/src/http/Response.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Sectheater\http;
- 
-class Response
-{
-    
-}
